ee0b4e8b3ab56fbadc2203f60de58a6bb9550769
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2021-01-29 08:52:17 +0100

    desktop-venv-jupyter-link fine tuning

--- src/marburg_biobank/jupyter_venv_on_desktop.py
+++ src/marburg_biobank/jupyter_venv_on_desktop.py
@@ -5,10 +5,14 @@ from pathlib import Path
 
 venv_path = Path(sys.prefix)
 notebook_path = venv_path.parent
-if sys.platform == 'Win32':
-    jupyter_cmd = venv_path / 'Scripts' / 'jupyter.exe' # should use the venv's jupyter, right
+is_windows = sys.platform.lower() in ("win32", "windows")
+if is_windows:
+    jupyter_cmd = (
+        venv_path / "Scripts" / "jupyter.exe"
+    )  # should use the venv's jupyter, right
 else:
-    jupyter_cmd = venv_path / 'bin' / 'jupyter' # should use the venv's jupyter, right
+    jupyter_cmd = venv_path / "bin" / "jupyter"  # should use the venv's jupyter, right
+
 
 def disable_use_redirect_file():
     jupyter_config = Path("~/.jupyter/jupyter_notebook_config.py").expanduser()
@@ -20,26 +24,26 @@ def disable_use_redirect_file():
             op.write_text(fix)
     print("Disabled file based jupyter redirect")
 
+
 def place_shortcut_on_desktop():
-    if sys.platform == 'Win32':
-        cmd = 'PowerShell -NoProfile --Command "Write-Host([Environment]::GetFolderPath(\'Desktop\'))"'
-        desktop_folder = Path(subprocess.check_output(cmd, shell=True).decode('utf-8'))
-        bin = 'Scripts'
+    if is_windows:
+        cmd = "PowerShell -NoProfile --Command \"Write-Host([Environment]::GetFolderPath('Desktop'))\""
+        desktop_folder = Path(subprocess.check_output(cmd, shell=True).decode("utf-8"))
     else:
         desktop_folder = Path("~/Desktop").expanduser()
-        bin = 'bin'
-    target = desktop_folder / ("jupyter notebook " + notebook_path.name + '.py')
-    target.write_text(f"""#!/usr/bin/env python3
+    target = desktop_folder / ("jupyter notebook " + notebook_path.name + ".py")
+    target.write_text(
+        f"""#!/usr/bin/env python3
 import subprocess
 subprocess.call([r"{jupyter_cmd}", 'notebook'], cwd=r"{notebook_path}")
-""")
-    if sys.platform != 'Win32':
+"""
+    )
+    if is_windows:
         os.chmod(target, 0o755)
     print("Placed jupyter shortcut on desktop")
 
+
 def main():
-    if sys.platform == 'Win32':
+    if sys.platform == "Win32":
         disable_use_redirect_file()
     place_shortcut_on_desktop()
-
-
